5420277ca777e3d28d30eac06365339de5556dae
Michel <Michelgomesdes@hotmail.com>
2015-08-06 01:41:44 -0300

    Ajustando os botoes dos Serviços

--- index.php
+++ index.php
@@ -79,14 +79,14 @@
 					<img src="img/responsive.png" alt="Design de Sites">
 					<p>Fazemos sites com elegância e muita qualidade, com um atendimento de ponta e especializado.</p>
 						<div id="botao2">
-							<p><a href="#">Web Designer</a></p>
+							<p><a href="#contato">Web Designer</a></p>
 						</div>
 				</div>
 				<div class="servico2">
 					<img src="img/hosting.png" alt="Hospedagem">
 					<p>Você pode hospedar seu site conosco, com qualidade, segurança, disponibilidade 24 horas sem quedas.</p>
 						<div id="botao1">
-							<p><a href="#">Hospedagem</a></p>
+							<p><a href="#contato" >Hospedagem</a></p>
 						</div>
 				</div>
 				<div class="clear"></div>
@@ -177,60 +177,61 @@
 
 
 				<h2>Contato</h2>
-				
+
 				<?php
-				if (isset($_POST['enviar'])) {
-					//code email here
-					$nome = $_POST['nome'];
-					$email = $_POST['email'];
-					$mensagem = $_POST['mensagem'];
-					// Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
-					require_once("phpmailer/class.phpmailer.php");
-					include 'phpmailer/PHPMailerAutoload.php';
-					// Inicia a classe PHPMailer
-					$mail = new PHPMailer();
-					// Define os dados do servidor e tipo de conexão
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					$mail->IsSMTP(); // Define que a mensagem será SMTP
-					$mail->Host = "host.muxdigital.com.br"; // Endereço do servidor SMTP
-					$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
-					$mail->Username = 'contato@clavierdev.com'; // Usuário do servidor SMTP
-					$mail->Password = 'clavier2015'; // Senha do servidor SMTP
-					// Define o remetente
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					$mail->From = $email; // Seu e-mail
-					$mail->FromName = $email; // Seu nome
-					// Define os destinatário(s)
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					$mail->AddAddress('contato@clavierdev.com', 'ClavierDev Contato');
-					//$mail->AddAddress('ciclano@site.net');
-					//$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
-					//$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
-					// Define os dados técnicos da Mensagem
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-					$mail->CharSet = 'utf-8'; // Charset da mensagem (opcional)
-					// Define a mensagem (Texto e Assunto)
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					$mail->Subject  = "Enviado pelo Formulario"; // Assunto da mensagem
-					$mail->Body = $mensagem;
-					$mail->AltBody = ""; 
-					// Define os anexos (opcional)
-					// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-					//$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
-					// Envia o e-mail
-					$enviado = $mail->Send();
-					// Limpa os destinatários e os anexos
-					$mail->ClearAllRecipients();
-					$mail->ClearAttachments();
-					// Exibe uma mensagem de resultado
-					if ($enviado) {
-					  echo "<h1>E-mail enviado com sucesso!</h1>";
-					} else {
-					  echo "Não foi possível enviar o e-mail.";
-					  echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
+					if (isset($_POST['enviar'])) {
+						//code email here
+						$nome = $_POST['nome'];
+						$email = $_POST['email'];
+						$mensagem = $_POST['mensagem'];
+						
+						// Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
+						require_once("phpmailer/class.phpmailer.php");
+						include 'phpmailer/PHPMailerAutoload.php';
+						// Inicia a classe PHPMailer
+						$mail = new PHPMailer();
+						// Define os dados do servidor e tipo de conexão
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						$mail->IsSMTP(); // Define que a mensagem será SMTP
+						$mail->Host = "host.muxdigital.com.br"; // Endereço do servidor SMTP
+						$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
+						$mail->Username = 'contato@clavierdev.com'; // Usuário do servidor SMTP
+						$mail->Password = 'clavier2015'; // Senha do servidor SMTP
+						// Define o remetente
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						$mail->From = $email; // Seu e-mail
+						$mail->FromName = $email; // Seu nome
+						// Define os destinatário(s)
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						$mail->AddAddress('contato@clavierdev.com', 'ClavierDev Contato');
+						//$mail->AddAddress('ciclano@site.net');
+						//$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
+						//$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
+						// Define os dados técnicos da Mensagem
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
+						$mail->CharSet = 'utf-8'; // Charset da mensagem (opcional)
+						// Define a mensagem (Texto e Assunto)
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						$mail->Subject  = "Enviado pelo Formulario"; // Assunto da mensagem
+						$mail->Body = $mensagem;
+						$mail->AltBody = ""; 
+						// Define os anexos (opcional)
+						// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+						//$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
+						// Envia o e-mail
+						$enviado = $mail->Send();
+						// Limpa os destinatários e os anexos
+						$mail->ClearAllRecipients();
+						$mail->ClearAttachments();
+						// Exibe uma mensagem de resultado
+						if ($enviado) {
+						  echo "<h1>E-mail enviado com sucesso!</h1>";
+						} else {
+						  echo "Não foi possível enviar o e-mail.";
+						  echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
+						}
 					}
-				}
 				?>
 				<form action="index.php#contato" method="POST"><br><p>
 				  <b>Nome: <br><input type="text" width="50px" name="nome" style="
